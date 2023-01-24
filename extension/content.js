3991d5b253a7d2aab22dae2afafdbe4e311b5e9e
curddd <admin@chatter.today>
2023-01-24 13:22:12 +0100

    manifest v3, hashed urls

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,6 @@
 //CHAT ENTRY WINDOW
 var div = document.createElement("div");
-div.style.cssText = "position: absolute;bottom: 10px;right: 10px;width: 200px;background-color: rgb(229, 229, 229);text-align: center;";
+div.style.cssText = "position: fixed;bottom: 10px;right: 10px;width: 200px;background-color: rgb(229, 229, 229);text-align: center;z-index:999";
 document.body.appendChild(div);
 
 //LOAD STYLED CHAT
@@ -15,9 +15,9 @@ fetch(chrome.runtime.getURL('chat.html'))
 //CHAT FUNCTIONALITY
 function bindChat(){
     
-    const messagesDiv = document.getElementById('messages');
-    const messageInput = document.getElementById('message-input');
-    const sendButton = document.getElementById('send-button');
+    const messagesDiv = document.getElementById('messages-3406983049683046');
+    const messageInput = document.getElementById('message-input-3406983049683046');
+    const sendButton = document.getElementById('send-button-3406983049683046');
     
     sendButton.addEventListener('click', function() {
         chrome.runtime.sendMessage({type: 'SAY', channel: window.location.href, message: messageInput.value});
